a6a9b280534f6ef0aeecfd84f14e95220bb2a140
david <fremenida@gmail.com>
2020-07-06 19:59:29 +0200

    nuevo

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 $(document).ready(()=>{
     $.ajax({
-        url: "https://www.el-tiempo.net/api/json/v2/home";
+        url: "https://www.el-tiempo.net/api/json/v2/home",
         success:function (response){
            console.log(response);
         },
-        eeror:function (err) {
+        error:function (err) {
             console.error();  
         }
         
