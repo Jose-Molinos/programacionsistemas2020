fe6fea874a2ffb492cb173b5ce7fedc21f147b6a
david <fremenida@gmail.com>
2020-07-06 20:12:09 +0200

    actual tiempo

--- js/index.js
+++ js/index.js
@@ -3,9 +3,11 @@ $(document).ready(()=>{
         url: "https://www.el-tiempo.net/api/json/v2/home",
         success:function (response){
            console.log(response);
+           $("#tiempo").text(response.tittle)  
         },
         error:function (err) {
-            console.error();  
+            console.error();
+           
         }
         
         
