8776d047950daead82148441291b49c7b7f44e5c
Quique <ejfdezrdgz@gmail.com>
2020-07-07 20:35:34 +0200

    Changed weather widget format

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-$(document).ready(()=>{
-$.ajax({
-    url: 'https://www.el-tiempo.net/api/json/v2/provincias/36/municipios/36057',
-	success: function(respuesta) {
-        console.log(respuesta);
-        $('#tiempo').text("Estado del cielo : "+respuesta.stateSky.description)
-        $('#temperatura-min').text("Temperatura minima : "+respuesta.temperaturas.min)
-        $('#temperatura-max').text("Temperatura maxima : "+respuesta.temperaturas.max)
-
-	},
-	error: function() {
-        console.log("No se ha podido obtener la información");
-    }
+$(document).ready(() => {
+    $.ajax({
+        url:
+            "https://www.el-tiempo.net/api/json/v2/provincias/36/municipios/36057",
+        success: function (respuesta) {
+            console.log(respuesta);
+            $("#tiempo").text(
+                "Estado del cielo: " + respuesta.stateSky.description
+            );
+            $("#temperatura-min").text(
+                "Temperatura minima: " + respuesta.temperaturas.min + " ºC"
+            );
+            $("#temperatura-max").text(
+                "Temperatura maxima: " + respuesta.temperaturas.max + " ºC"
+            );
+        },
+        error: function () {
+            console.log("No se ha podido obtener la información");
+        },
+    });
 });
-}
-);
